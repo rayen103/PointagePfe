b2a6e200eeba4eb2dcc3e28ed459b65def47ce4d
rayen103 <rayen.farhani@esprit.tn>
2026-09-13 14:35:32 +0100

    ci: configure automated GitHub Actions deployment to Azure App Service

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    BaseApi:'http://41.227.16.17:6867/cm/',
-    // BaseApi:'http://localhost:6064/cm/',
+    BaseApi: 'https://api-pointage-rayen.azurewebsites.net/cm/',
     mapGeocodingApi: 'https://nominatim.openstreetmap.org/search',
 };
